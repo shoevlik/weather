35265f6ae41b4f7f71b8cec23f8765fed16ef4d6
Kate Alikina <ketrinpups@gmail.com>
2025-04-23 10:34:18 +0300

    Добавлены изменения

--- WeatherService/JsonService.h
+++ WeatherService/JsonService.h
@@ -9,26 +9,8 @@ using namespace std;
 
 class JsonService : public Service
 {
-	virtual Weather getWeather(std::string s) override;
-	virtual ~JsonService() {};
-    Weather getWeather(std::string s)
-    {
-        std::ifstream fin(s);
-        if (!fin)
-            throw exception("error");
-
-        json j;
-        j = json::parse(fin);
-        std::string city = j["name"]; 
-        double lon = j["coord"]["lon"]; 
-        double lat = j["coord"]["lat"];   
-        double temperature = j["main"]["temp"]; 
-        std::string weather = j["weather"]["main"]; 
-        double windSpeed = j["wind"]["speed"]; 
-        int clouds = j["clouds"]["all"]; 
-        return Weather(city,lon,lat,temperature,weather,windSpeed,clouds);
-    }
-
-	
+public:
+    virtual Weather getWeather(std::string s) override;
+    virtual ~JsonService() {};
 };
 
--- WeatherService/Service.h
+++ WeatherService/Service.h
@@ -6,6 +6,6 @@ class Service
 {
 public:
 	virtual Weather getWeather(std::string s) = 0;
-	virtual ~Service();
+	virtual ~Service() {};
 };
 
